94567c84c34fefa0a46e316f9be4e7783c20ee9c
David Johnson <djohnson.m@gmail.com>
2017-12-02 09:39:25 -0800

    Solved part b

--- 2.hs
+++ 2.hs
@@ -1,14 +1,18 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 
-main = print =<< two
+main = print =<< two go
 
-two :: IO Int
-two =
+b = print =<< two partB
+
+two k =
     sum .
-    map go .
+    map k .
     fmap (map read) .
     map words .
     lines <$> readFile "2.txt"
 
 go :: [Int] -> Int
 go xs = maximum xs - minimum xs
+
+partB :: [Int] -> Int
+partB xs = head [ x `div` y | x <- xs, y <- xs, x `mod` y == 0, x /= y ]
