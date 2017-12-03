28d1fde40c7612a611e1796f24769f53349c4f6a
David Johnson <djohnson.m@gmail.com>
2017-12-03 01:03:23 -0800

    go faster!

--- 3.hs
+++ 3.hs
@@ -1,7 +1,12 @@
-module Three where
+{-# LANGUAGE FlexibleContexts #-}
+{-#LANGUAGE BangPatterns#-}
+module Main where
 
 import           Data.Map    (Map)
-import qualified Data.Map    as M
+import qualified Data.Map.Strict as M
+
+main :: IO ()
+main = print partA
 
 squares :: [[Int]]
 squares = [1] : lists genCorner
@@ -11,14 +16,14 @@ squares = [1] : lists genCorner
     lists (x:y:xs) = drop 1 [x..y] : lists (y:xs)
     genCorner = map ((+1) . (*8)) (go 0 1)
       where
-        go n _ | n >= 289326 = []
-        go n k = n : go (n + k) (k + 1)
+        go !n _ | n >= 289326 = []
+        go !n !k = n : go (n + k) (k + 1)
 
 data Dir = U | L | R | D
   deriving (Show, Enum)
 
 partA :: Int
-partA = abs x + abs y + 1
+partA = abs x + abs y
   where
    (x,y) = resultMap M.! 289326
 
@@ -43,16 +48,20 @@ resultMap = go squares (0,0,1,mempty)
       shiftCoords rs moves
 
     shiftCoords rs [] = rs
-    shiftCoords (x,y,k,map') ((n,dir):xs) = do
+    shiftCoords (!x,!y,!k,!map') ((!n,!dir):xs) = do
       shiftCoords ( newX
                   , newY
                   , newN
-                  , M.insert newN (newX,newY) map'
+                  , M.insert k (x, y) map'
                   ) xs
         where
-         (newX, newY, newN) =
+         (!newX, !newY, !newN) =
            case dir of
              U -> (x,y+1,n+1)
              D -> (x,y-1,n+1)
              L -> (x-1,y,n+1)
              R -> (x+1,y,n+1)
+
+partB = undefined
+
+
