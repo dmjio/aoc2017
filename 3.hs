a408e25212c914cf2d242cc458a8852c17a5455e
David Johnson <djohnson.m@gmail.com>
2017-12-03 02:29:56 -0800

    add part b

--- 3.hs
+++ 3.hs
@@ -2,20 +2,21 @@
 {-#LANGUAGE BangPatterns#-}
 module Main where
 
+import           Control.Monad
 import           Data.Map        (Map)
 import qualified Data.Map.Strict as M
+import           Data.Maybe
 import           Debug.Trace
 
-main :: IO ()
-main = print partA
+main = print partA >> print partB
 
 squares :: [[Int]]
-squares = [1] : lists genCorner
+squares = init $ [1] : lists genCorner
   where
     lists [] = []
     lists [x] = [[x]]
     lists (x:y:xs) = drop 1 [x..y] : lists (y:xs)
-    genCorner = map ((+1) . (*8)) (go 0 1)
+    genCorner = map ((+1).(*8)) (go 0 1)
       where
         go !n _ | n >= 289326 = []
         go !n !k = n : go (n + k) (k + 1)
@@ -28,18 +29,12 @@ partA = abs x + abs y
   where
    (x,y) = resultMap M.! 289326
 
-flippedMap :: Map (Int, Int) Int
-flippedMap =
-  M.fromList [ (v,k)
-             | (k,v) <- M.assocs resultMap
-             ]
-
 resultMap :: Map Int (Int, Int)
-resultMap = go squares (0,0,1,M.singleton 1 (0,0))
+resultMap = go squares (0, 0, 1, M.singleton 1 (0,0))
   where
     go [] (_,_,_,map') = map'
     go (xs:xxs) rs =
-      go xxs (calcMoves xs rs)
+      go xxs $! (calcMoves xs rs)
 
     calcMoves xs rs@(x,y,k,_) = do
       let moveCount = length xs `div` 4
@@ -69,6 +64,61 @@ resultMap = go squares (0,0,1,M.singleton 1 (0,0))
              L -> (x-1,y)
              R -> (x+1,y)
 
-partB = undefined
+partB :: Int
+partB = go squares (0, 0, 1, M.singleton (0,0) 1, 0)
+  where
+    go _ (_,_,_,_,x) | x >= 289326 = x
+    go [] (_,_,_,_,x) = x
+    go (xs:xxs) rs = go xxs $! calcMoves xs rs
+
+    calcMoves xs rs = do
+      let moveCount = length xs `div` 4
+          moveRight = [ R | moveCount /= 0 ]
+          moveDirs  =
+            concat [ moveRight
+                   , replicate (moveCount - 1) U
+                   , replicate moveCount L
+                   , replicate moveCount D
+                   , replicate moveCount R
+                   ]
+          moves = zip xs moveDirs
+      shiftCoords rs moves
+
+    shiftCoords rs [] = rs
+    shiftCoords rs@(_,_,_,_,!max') _ | max' >= 289326 = rs
+    shiftCoords (!x,!y,!k,!map',!max') ((!n,!dir):xs) = do
+      let (newMax, newMap) = specialInsert (newX, newY) n map'
+      shiftCoords ( newX
+                  , newY
+                  , n
+                  , newMap
+                  , newMax
+                  ) xs
+        where
+         (!newX, !newY) =
+           case dir of
+             U -> (x,y+1)
+             D -> (x,y-1)
+             L -> (x-1,y)
+             R -> (x+1,y)
+
+    specialInsert (x,y) n map' = do
+      let sum = getAdjSum (x,y) map'
+      (sum, M.insert (x,y) sum map')
+
+    getAdjSum (x,y) map' = sum (catMaybes ls)
+      where
+        ls = [
+             M.lookup (x+1,y) map'
+           , M.lookup (x,y+1) map'
+           , M.lookup (x+1,y+1) map'
+
+           , M.lookup (x-1,y) map'
+           , M.lookup (x,y-1) map'
+           , M.lookup (x-1,y-1) map'
+
+           , M.lookup (x+1,y-1) map'
+           , M.lookup (x-1,y+1) map'
+           ]
 
 
