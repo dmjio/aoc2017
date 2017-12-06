13b4051e6f94de76a484c945d41867329610df06
David Johnson <djohnson.m@gmail.com>
2017-12-06 01:28:50 -0800

    Simplify step

--- 6.hs
+++ 6.hs
@@ -15,12 +15,23 @@ import qualified Data.Sequence       as S
 import qualified Data.Vector         as V
 import           Debug.Trace
 
+step :: V.Vector Int -> V.Vector Int
+step vec = snd $ iterate go (maxIdx + 1, vec V.// [(maxIdx,0)]) !! maxVal
+  where
+    maxIdx = V.maxIndex vec
+    maxVal = V.maximum vec
+    go (idx, vec) = (newIdx + 1, vec V.// xs)
+      where
+        newIdx = idx `mod` V.length vec
+        val = vec V.! newIdx
+        xs = [(newIdx, val + 1)]
+
 main :: IO ()
 main = do
   rs :: V.Vector Int <- V.fromList . map read . words <$> readFile "6.txt"
   () <- flip evalStateT (0,rs, [rs], False) $ fix $ \loop -> do
     (!seenCount, rs', others, hasSeen) <- get
-    let newRs = updateVec rs'
+    let newRs = step rs'
     if newRs `elem` others
       then do
         let Just ixxx' = elemIndex newRs others
@@ -38,32 +49,9 @@ main = do
         loop
   print ()
 
-updateVec :: V.Vector Int -> V.Vector Int
-updateVec xs = do
-   let val' = V.maximum xs
-       index = V.maxIndex xs
-       newRs' :: V.Vector Int = xs & ix index .~ 0
-   let (_, kk, _) = flip execState (val', newRs', index + 1) $ fix $ \innerLoop -> do
-          (!v, ks, !ix') <- get
-          if v == 1
-            then do
-              let newKs' = ks & ix ix' %~ (+1)
-              put (v, newKs', ix' + 1)
-            else if ix' >= length ks - 1
-                 then do
-                    let newKs' = ks & ix ix' %~ (+1)
-                    put (if ix' > length ks - 1 then v else v - 1, newKs', 0)
-                    innerLoop
-                 else do
-                    let newKs' = ks & ix ix' %~ (+1)
-                    put (v - 1, newKs', ix'+1)
-                    innerLoop
-   kk
-
 -- unit tests
-z = updateVec (V.fromList [0,2,7,0]) == V.fromList [2,4,1,2]
-z' = updateVec (V.fromList [2,4,1,2]) == V.fromList [3,1,2,3]
-z'' = updateVec (V.fromList [3,1,2,3]) == V.fromList [0,2,3,4]
-z''' = updateVec (V.fromList [0,2,3,4]) == V.fromList [1,3,4,1]
-
-kk = and [z,z',z'',z''']
+z = step (V.fromList [0,2,7,0]) == V.fromList [2,4,1,2]
+z' = step (V.fromList [2,4,1,2]) == V.fromList [3,1,2,3]
+z'' = step (V.fromList [3,1,2,3]) == V.fromList [0,2,3,4]
+z''' = step (V.fromList [0,2,3,4]) == V.fromList [1,3,4,1]
+tests = and [z,z',z'',z''']
