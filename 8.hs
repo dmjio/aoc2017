b787ba1773553de5ff2b58faf1af88bc714605a6
David Johnson <djohnson.m@gmail.com>
2017-12-07 23:48:50 -0800

    clean house on 8

--- 8.hs
+++ 8.hs
@@ -1,30 +1,16 @@
-{-# LANGUAGE FlexibleContexts    #-}
-{-# LANGUAGE RecordWildCards     #-}
-{-# LANGUAGE OverloadedStrings   #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import           Control.Lens
-import           Control.Monad
-import           Data.Char
-import           Data.Function
-import           Data.List
-import           Data.List.Split
 import qualified Data.Map        as M
-import           Data.Maybe
-import           Data.Set
-import           Data.Tree
-import qualified Data.Vector     as V
 
 main :: IO ()
 main = do
-  rs :: [String] <- lines <$> readFile "8.txt"
+  rs <- lines <$> readFile "8.txt"
   let (partB, partA) = parse rs (populate M.empty rs)
   mapM_ print [ partA, partB ]
 
 -- | Populate empty map w/ registers initialized to 0
 populate :: M.Map String Int -> [String] -> M.Map String Int
-populate m rs = mconcat $ Prelude.map go rs
+populate m rs = mconcat (map go rs)
   where
     go :: String -> M.Map String Int
     go x = M.insert (words x !! 0) 0 m
@@ -46,13 +32,14 @@ parse rs = go rs 0
           go xs highest (M.alter f regName m)
             where
               highest = max (maximum m) h
-              f Nothing = Nothing
+              f Nothing = Just 0
               f (Just regValue) = do
                 let (#%#) = toCond cond
-                pure $ if m M.! regName2 #%# read pred
-                         then case op of
-                                "inc" -> regValue + read val
-                                "dec" -> regValue - read val
+                pure $ if M.findWithDefault 0 regName2 m #%# read pred
+                         then
+                           case op of
+                             "inc" -> regValue + read val
+                             "dec" -> regValue - read val
                          else regValue
 
 -- | Create conditional operation
